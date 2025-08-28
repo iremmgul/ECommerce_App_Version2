e119146c668ed77eb71fe0246d87f8d00102a6b9
iremmgul <irmgl88@gmail.com>
2025-08-28 18:02:43 +0300

    New details added for product

---
+++ ECommerceApi/Dtos/ProductReviewDto.cs
@@ -0,0 +1,13 @@
+﻿namespace ECommerceApi.Dtos
+{
+    public class ProductReviewDto
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int UserId { get; set; }
+        public int Rating { get; set; }
+        public string Comment { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+    }
+
+}
---
+++ ECommerceApi/Models/ProductReview.cs
@@ -0,0 +1,18 @@
+﻿namespace ECommerceApi.Models
+{
+    public class ProductReview
+    {
+        public int Id { get; set; }
+
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public int Rating { get; set; }
+        public string Comment { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+
+}
